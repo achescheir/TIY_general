005fcf0e9e3db83621a7b909d84c749a5f1ed680
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-03-28 14:52:06 -0400

    Epic mode works, needs refinement.

--- hw1/input_stats.py
+++ hw1/input_stats.py
@@ -2,6 +2,53 @@
 # store input numbers
 # output count, sum, avg of numbers
 # handle non numerics
+from statistics import mean, median, mode, stdev, StatisticsError
+
+def min_max_avg_string(strings):
+    lengths =[]
+    for string in strings:
+        lengths.append(len(string))
+    return (min(lengths),max(lengths),(sum(lengths)/len(lengths)))
+
+def character_counter(string):
+    characters = {}
+    for character in string:
+        if character in characters:
+            characters[character]+=1
+        else:
+            characters[character]=1
+    return characters
+
+def display_numbers(numbers):
+    print("You input {} numbers.".format(len(numbers)))
+    print("The numbers sum to {}.".format(sum(numbers)))
+    if len(numbers)== 0:
+        print("Average, mean, meadian, mode, and standard deviation are undefined for 0 numbers.")
+    else:
+        print("Their {} is {}.".format("average",sum(numbers)/len(numbers)))
+        print("Their {} is {}.".format("mean",mean(numbers)))
+        print("Their {} is {}.".format('median',median(numbers)))
+        try:
+            print("Their {} is {}.".format('mode',mode(numbers)))
+        except StatisticsError:
+            print("This list has no mode.")
+        print("Their {} is {}.".format('standard deviation',stdev(numbers)))
+
+
+def display_strings(strings):
+    string_min,string_max,string_avg = min_max_avg_string(strings)
+    all_together =''.join(strings)
+    characters = character_counter(all_together)
+    print(all_together)
+    print("That was {} separate strings.".format(len(strings)))
+    print("The shortest string was {} characters.".format(string_min))
+    print("The longest string was {} characters.".format(string_max))
+    print("The average string was {} character.".format(string_avg))
+    if 'e' in characters:
+        print("The letter 'e' was used {} times.".format(characters['e']))
+    else:
+        print("The letter 'e' was never used.")
+
 def number_list(inputs):
     print("This will be a list of numbers.")
     while True:
@@ -14,12 +61,7 @@ def number_list(inputs):
             except ValueError:
                 print("That was not a number.")
                 continue
-    print("You input {} numbers.".format(len(inputs)))
-    print("The numbers sum to{}.".format(sum(inputs)))
-    if len(inputs)== 0:
-        print("Average is undefined for 0 numbers.")
-    else:
-        print("Their average is {}.".format(sum(inputs)/len(inputs)))
+    display_numbers(inputs)
 
 def string_list(inputs):
     print("This will be a list of strings.")
@@ -29,7 +71,7 @@ def string_list(inputs):
             break
         else:
             inputs.append(user_in)
-    print(''.join(inputs))
+    display_strings(inputs)
 
 
 inputs = []
