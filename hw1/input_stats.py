aaf74b85d70c20c2ebde75feee0a02b4dc5ed3f2
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-03-28 13:55:59 -0400

    Hard mode works

--- hw1/input_stats.py
+++ hw1/input_stats.py
@@ -2,17 +2,45 @@
 # store input numbers
 # output count, sum, avg of numbers
 # handle non numerics
-numbers = []
-while True:
-    user_in = input("Please input a number. Blank line to end: ")
-    if user_in == "":
-        break
+def number_list(inputs):
+    print("This will be a list of numbers.")
+    while True:
+        user_in = input("Please input a number. Blank line to end: ")
+        if user_in == "":
+            break
+        else:
+            try:
+                inputs.append(float(user_in))
+            except ValueError:
+                print("That was not a number.")
+                continue
+    print("You input {} numbers.".format(len(inputs)))
+    print("The numbers sum to{}.".format(sum(inputs)))
+    if len(inputs)== 0:
+        print("Average is undefined for 0 numbers.")
     else:
-        try:
-            numbers.append(float(user_in))
-        except ValueError:
-            print("That was not a number.")
-            continue
-print("You input {} numbers.".format(len(numbers)))
-print("The numbers sum to{}.".format(sum(numbers)))
-print("Ther average is {}".format(sum(numbers)/len(numbers)))
+        print("Their average is {}.".format(sum(inputs)/len(inputs)))
+
+def string_list(inputs):
+    print("This will be a list of strings.")
+    while(True):
+        user_in = input("Please input a value. Blank line to end:")
+        if user_in =="":
+            break
+        else:
+            inputs.append(user_in)
+    print(''.join(inputs))
+
+
+inputs = []
+first_in = input("Please input a value. Blank line to end:")
+if first_in == "":
+    pass
+else:
+    try:
+        inputs.append(float(first_in))
+    except ValueError:
+        string_list([first_in])
+    else:
+        number_list(inputs)
+print("Goodbye.")
